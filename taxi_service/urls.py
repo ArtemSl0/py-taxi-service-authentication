125ff27f03fa0d9e72280413acdef57c5fec28c3
Artem <slartem2007@gmail.com>
2026-09-25 22:46:10 +0300

    Solution(correct mistakes)

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
-from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(
         "login/",
         LoginView.as_view(template_name="registration/login.html"),
-        name="login"),
+        name="login",
+    ),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("", include("taxi.urls", namespace="taxi")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
